5482ebb0624c79512b31927c2e7571ac0afc1aaa
jadenhensley <jadencoding@gmail.com>
2023-03-18 13:22:46 -0400

    added jrpg game to launcher

--- game_data.py
+++ game_data.py
@@ -11,6 +11,12 @@ game_data = {
             "title":"Snake",
             "icon":"games/snake/icon.png",
             "executable":"games/snake/snake.py"
+        },
+        "jrpg_prototype": {
+            "repository":"https://github.com/jadenhensley/jrpg_prototype",
+            "title":"JRPGPrototype",
+            "icon":"games/jrpg_prototype/icon.png",
+            "executable":"games/jrpg_prototype/main.py"
         }
     }
 }
